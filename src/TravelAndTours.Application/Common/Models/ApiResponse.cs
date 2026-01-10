9bc1324a0d8e662cf1d0bb8f748819625c163a32
kshetrastha <42167216+kshetrastha@users.noreply.github.com>
2026-01-10 21:45:06 +0545

    Add ApiResponse model

---
+++ src/TravelAndTours.Application/Common/Models/ApiResponse.cs
@@ -0,0 +1,26 @@
+namespace TravelAndTours.Application.Common.Models;
+
+public class ApiResponse<T>
+{
+    public bool Success { get; init; }
+    public string Message { get; init; } = string.Empty;
+    public T? Data { get; init; }
+    public List<string> Errors { get; init; } = [];
+
+    public static ApiResponse<T> Ok(T data, string message = "Success")
+        => new()
+        {
+            Success = true,
+            Message = message,
+            Data = data
+        };
+
+    public static ApiResponse<T> Fail(string message, List<string>? errors = null, T? data = default)
+        => new()
+        {
+            Success = false,
+            Message = message,
+            Data = data,
+            Errors = errors ?? []
+        };
+}
